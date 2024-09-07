2401482573a74be93431218621e19a7c0cb7a5d9
Hugo <smahlij@gmail.com>
2024-09-06 23:06:59 -0300

    Agregado de validaciones para jornadas laborales

--- src/main/java/com/hugo/api/repository/JornadaLaboralRepository.java
+++ src/main/java/com/hugo/api/repository/JornadaLaboralRepository.java
@@ -1,9 +1,24 @@
 package com.hugo.api.repository;
 
+import com.hugo.api.entity.ConceptoLaboral;
+import com.hugo.api.entity.Empleado;
 import com.hugo.api.entity.JornadaLaboral;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface JornadaLaboralRepository extends JpaRepository<JornadaLaboral, Integer> {
+    List<JornadaLaboral> findByEmpleadoAndFecha(Empleado empleado, LocalDate date);
+    List<JornadaLaboral> findByEmpleadoAndFechaBetween(Empleado empleado, LocalDate inicioSemana, LocalDate finSemana);
+    Optional<JornadaLaboral> findByEmpleadoAndFechaAndConceptoLaboralId(Empleado empleado, LocalDate fecha, Integer idConcepto);
+    @Query("SELECT COUNT(j) FROM JornadaLaboral j WHERE j.conceptoLaboral = :conceptoLaboral AND j.fecha = :fecha")
+    long countByConceptoLaboralAndFecha(@Param("conceptoLaboral") ConceptoLaboral conceptoLaboral, @Param("fecha") LocalDate fecha);
+    @Query("SELECT j FROM JornadaLaboral j WHERE j.empleado = :empleado AND j.fecha = :fecha AND j.conceptoLaboral.id = :conceptoId")
+    Optional<JornadaLaboral> findByEmpleadoAndFechaAndConceptoLaboral(@Param("empleado") Empleado empleado,@Param("fecha") LocalDate fecha,@Param("conceptoId") Integer conceptoId);
 }
--- src/main/java/com/hugo/api/service/impl/JornadaLaboralServiceImpl.java
+++ src/main/java/com/hugo/api/service/impl/JornadaLaboralServiceImpl.java
@@ -9,9 +9,15 @@ import com.hugo.api.repository.ConceptoLaboralRepository;
 import com.hugo.api.repository.EmpleadoRepository;
 import com.hugo.api.repository.JornadaLaboralRepository;
 import com.hugo.api.service.JornadaLaboralService;
+import org.springframework.cglib.core.Local;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
+
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class JornadaLaboralServiceImpl implements JornadaLaboralService {
@@ -35,6 +41,127 @@ public class JornadaLaboralServiceImpl implements JornadaLaboralService {
         ConceptoLaboral conceptoLaboral = conceptoLaboralRepository.findById(jornadaLaboralDTO.getIdConcepto())
                 .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "No existe el concepto ingresado."));
 
+
+        // Regla 1 - Validacion de rango de horas por concepto laboral
+        Integer horasTrabajadas = jornadaLaboralDTO.getHorasTrabajadas();
+        if (horasTrabajadas != null) {
+            Integer hsMinimo = conceptoLaboral.getHsMinimo();
+            Integer hsMaximo = conceptoLaboral.getHsMaximo();
+
+            if (hsMinimo != null && hsMaximo != null) {
+                if (horasTrabajadas < conceptoLaboral.getHsMinimo() || horasTrabajadas > conceptoLaboral.getHsMaximo()){
+                    throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "El rango de horas que se puede cargar para este concepto es de " + conceptoLaboral.getHsMinimo() + " - " + conceptoLaboral.getHsMaximo());
+                }
+            }
+        }
+
+        // Regla 2 - Validacion de suma de horas en el día por empleado
+        LocalDate fecha = jornadaLaboralDTO.getFecha();
+        List<JornadaLaboral> jornadasPorDia = jornadaLaboralRepository.findByEmpleadoAndFecha(empleado, fecha);
+
+        int horasTotales = jornadasPorDia.stream()
+                .mapToInt(JornadaLaboral::getHorasTrabajadas)
+                .sum();
+
+        if (horasTotales + jornadaLaboralDTO.getHorasTrabajadas() > 14) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Un empleado no puede cargar más de 14 horas trabajadas por día.");
+        }
+
+        // Regla 3 - Horas semanales por empleado (lunes a viernes)
+        LocalDate inicioSemana = fecha.with(DayOfWeek.MONDAY);
+        LocalDate finSemana = fecha.with(DayOfWeek.FRIDAY);
+
+        List<JornadaLaboral> jornadaEmpleadoSemana = jornadaLaboralRepository.findByEmpleadoAndFechaBetween(empleado, inicioSemana, finSemana);
+
+        // Filtrar las jornadas de lunes a viernes
+        int totalHorasSemanales = jornadaEmpleadoSemana.stream()
+                .filter(jornada -> jornada.getFecha().getDayOfWeek().getValue() >= DayOfWeek.MONDAY.getValue()
+                        && jornada.getFecha().getDayOfWeek().getValue() <= DayOfWeek.FRIDAY.getValue())
+                .mapToInt(JornadaLaboral::getHorasTrabajadas)
+                .sum();
+
+        // Verificar si el empleado supera las 52 horas semanales
+        if (totalHorasSemanales + jornadaLaboralDTO.getHorasTrabajadas() > 52) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "El empleado ingresado supera las 52 horas semanales.");
+        }
+
+        // Regla 4 - No más de 190 horas mensuales por empleado
+        LocalDate inicioMes = fecha.withDayOfMonth(1);
+        LocalDate finMes = fecha.withDayOfMonth(fecha.lengthOfMonth());
+
+        List<JornadaLaboral> jornadasEmpleadoMes = jornadaLaboralRepository.findByEmpleadoAndFechaBetween(empleado, inicioMes,finMes);
+
+        int totalHorasMensuales = jornadasEmpleadoMes.stream()
+                .mapToInt(JornadaLaboral::getHorasTrabajadas)
+                .sum();
+
+        if (totalHorasSemanales + jornadaLaboralDTO.getHorasTrabajadas() > 190) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "El empleado ingresado supera las 190 horas mensuales.");
+        }
+
+        // Regla 5 - Verificar que no tenga dia libre en la fecha
+        boolean tieneDiaLibre = jornadaLaboralRepository.findByEmpleadoAndFechaAndConceptoLaboralId(empleado, fecha, 3).isPresent();
+        System.out.println("Concepto Laboral: " + conceptoLaboral.getId());
+
+        if (tieneDiaLibre) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "El empleado ingresado ya cuenta con un día libre en esa fecha.");
+        }
+
+        // Regla 6 - No más de 3 turnos extra por semana
+        long turnosExtraSemana = jornadaEmpleadoSemana.stream()
+                .filter(jornada -> jornada.getConceptoLaboral().getId() == 2)
+                .count();
+
+        if (turnosExtraSemana >= 3) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "El empleado ingresado ya cuenta con 3 turnos extra esta semana.");
+        }
+
+        // Regla 7 - No más de 5 turnos normales por semana
+        long turnosNormalesSemana = jornadaEmpleadoSemana.stream()
+                .filter(jornada -> jornada.getConceptoLaboral().getId() == 1)
+                .count();
+
+        if (turnosNormalesSemana >= 5) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "El empleado ingresado ya cuenta con 5 turnos normales esta semana.");
+        }
+
+        // Regla 8 - Solo 2 dias libres por semana
+        long diasLibresSemana = jornadaEmpleadoSemana.stream()
+                .filter(jornada -> jornada.getConceptoLaboral().getId() == 3)
+                .map(JornadaLaboral::getFecha)
+                .distinct()
+                .count();
+
+        if (diasLibresSemana >= 2) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "El empleado no cuenta con más días libres esta semana.");
+        }
+
+        // Regla 9 - Solo 5 días libres al mes
+        long diasLibresMes = jornadasEmpleadoMes.stream()
+                .filter(jornada -> jornada.getConceptoLaboral().getId() == 3)
+                .map(JornadaLaboral::getFecha)
+                .distinct()
+                .count();
+
+        if (diasLibresMes >= 5) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "El empleado no cuenta con más días libres este mes.");
+        }
+
+        // Regla 10 - No más de 2 empleados por jornada
+        long empleadosPorJornada = jornadaLaboralRepository.countByConceptoLaboralAndFecha(conceptoLaboral,fecha);
+
+        if (empleadosPorJornada >= 2) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Ya existen 2 empleados registrados para este concepto en la fecha ingresada.");
+        }
+
+        // Regla 11 - No es posible cargar a un empleado con dos mismos conceptos de jornadas en el mismo día
+        boolean jornadaExistente = jornadaLaboralRepository.findByEmpleadoAndFechaAndConceptoLaboral(empleado, fecha, conceptoLaboral.getId()).isPresent();
+
+        if (jornadaExistente) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "El empleado ya tiene registrado una jornada con este concepto en la fecha ingresada.");
+        }
+
+        // CREACION DE JORNADA LABORAL
         JornadaLaboral jornadaLaboral = new JornadaLaboral();
         jornadaLaboral.setEmpleado(empleado);
         jornadaLaboral.setConceptoLaboral(conceptoLaboral);
